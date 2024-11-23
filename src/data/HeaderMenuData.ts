ad0dade90bea4c99014ddef0026047d7e5de259d
fabianlopezdev <fabianlopez@pm.me>
2024-11-23 09:40:46 -0500

    Header menu responsive with dynamic data

---
+++ src/data/HeaderMenuData.ts
@@ -0,0 +1,77 @@
+export const HEADER_MENU_LINKS = [
+  {
+    label: "La fundació",
+    url: "/",
+    ariaLabel: "",
+  },
+  {
+    label: "Un club amb valors",
+    url: "/",
+    ariaLabel: "",
+  },
+  {
+    label: "Palmarés del plater",
+    url: "/",
+    ariaLabel: "",
+  },
+  {
+    label: "Tornejos",
+    nested: [
+      {
+        label: "Handbol escolar",
+        url: "/",
+        ariaLabel: "",
+      },
+      {
+        label: "Minihandbol",
+        url: "/",
+        ariaLabel: "",
+      },
+      {
+        label: "Granollers Cup",
+        url: "/",
+        ariaLabel: "",
+      },
+    ],
+  },
+  {
+    label: "Equip especial",
+    url: "/",
+    ariaLabel: "",
+  },
+  {
+    label: "Projectes Especials",
+    nested: [
+      {
+        label: "Handbol escolar",
+        url: "/",
+        ariaLabel: "",
+      },
+      {
+        label: "Minihandbol",
+        url: "/",
+        ariaLabel: "",
+      },
+      {
+        label: "Granollers Cup",
+        url: "/",
+        ariaLabel: "",
+      },
+    ],
+  },
+  {
+    label: "La residència",
+    url: "/",
+    ariaLabel: "",
+  },
+  {
+    label: "Veterans",
+    url: "/",
+    ariaLabel: "",
+  },
+  {
+    label: "El llegat dels 25 anys",
+    url: "/",
+    ariaLabel: "",
+  },
+];
