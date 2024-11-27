d928e67f091d7cf05e0b525087d429cd063a33b3
fabianlopezdev <fabianlopez@pm.me>
2024-11-27 11:15:41 -0500

    Sections-display: adding each section dinamically

---
+++ src/data/SectionsData.ts
@@ -0,0 +1,66 @@
+import maskImage from "@assets/Mask-group.avif";
+import maskImage2 from "@assets/Mask-group-(1).webp";
+import maskImage3 from "@assets/Mask-group-(2).webp";
+import maskImage4 from "@assets/Mask-group-(3).avif";
+import maskImage5 from "@assets/Mask-group-(4).avif";
+import maskImage6 from "@assets/Mask-group-(5).avif";
+import maskImage7 from "@assets/Mask-group-(6).webp";
+import maskImage8 from "@assets/Mask-group-(7).webp";
+import maskImage9 from "@assets/Mask-group-(8).avif";
+
+export const SECTIONS = [
+    {
+        label: "Un club amb valors",
+        imageSrc: maskImage,
+        imageAlt: "",
+        linkHref: "/"
+    },
+    {
+        label: "Palmarés del planter",
+        imageSrc: maskImage2,
+        imageAlt: "",
+        linkHref: "/"
+    },
+    {
+        label: "Tornejos",
+        imageSrc: maskImage3,
+        imageAlt: "",
+        linkHref: "/"
+    },
+    {
+        label: "Equip Especial",
+        imageSrc: maskImage4,
+        imageAlt: "",
+        linkHref: "/"
+    },
+    {
+        label: "Projectes de cooperació",
+        imageSrc: maskImage5,
+        imageAlt: "",
+        linkHref: "/"
+    },
+    {
+        label: "La residència",
+        imageSrc: maskImage6,
+        imageAlt: "",
+        linkHref: "/"
+    },
+    {
+        label: "Veterans",
+        imageSrc: maskImage7,
+        imageAlt: "",
+        linkHref: "/"
+    },
+    {
+        label: "Acompanyant el club",
+        imageSrc: maskImage8,
+        imageAlt: "",
+        linkHref: "/"
+    },
+    {
+        label: "El llegat dels 25anys",
+        imageSrc: maskImage9,
+        imageAlt: "",
+        linkHref: "/"
+    }
+]
