c2bd6fabdfbe0628dfbe5a5a97bb2c8dbabed02c
fabianlopezdev <fabianlopez@pm.me>
2024-12-21 10:15:14 -0500

    La fundació page layout

---
+++ src/data/laFundacioData.ts
@@ -0,0 +1,25 @@
+import maskImage from "@assets/Mask-group-(1).webp";
+export const LA_FUNDACIO = {
+  title: "Fundació BM Granollers",
+  homePageMenuImg: {
+    imageSrc: maskImage, // Ensure maskImage is defined
+    imageAlt: "",
+  },
+  heroImage: { heroImageSrc: maskImage, heroImageAlt: "" },
+  sections: [
+    {
+      title: "La Fundació",
+      text: [
+        "L’1 de juny de 1999 es va constituir la Fundació BM Granollers, per iniciativa del Club Balonmano Granollers, i d’una sèrie de persones, empreses, entitats i institucions com l’Ajuntament de Granollers.",
+        "En un moment de canvis legislatius en l’àmbit de l’esport es plantejava la possibilitat que l’handbol de màxima categoria es convertís en una lliga professional. Això hauria obligat els clubs a transformar-se en societats anònimes. Clubs com el nostre es veien en la necessitat de reunir un capital social molt gran, amb el risc de perdre el seu sentit històric com a clubs de socis. En aquest context va néixer la Fundació BM Granollers, amb l'objectiu, segons l’article 4t. dels seus estatuts, de promoure l’esport de l’handbol en totes les seves categories, especialment les formatives. També es va comprometre a promocionar i donar suport a activitats esportives per a persones amb diversitat funcional.",
+        "D’altra banda, un punt clau i especialment rellevant, va ser la cessió del patrimoni del club a la Fundació Balonmano Granollers, amb l’objectiu de preservar-lo davant possibles escenaris de dificultat. Patrimoni com el de l’antiga pista d’handbol del carrer de Tetuan, finançada per particulars, on es van jugar partits durant les dècades dels cinquanta i els seixanta.",
+      ],
+    },
+    {
+      text: [
+        "Durant els seus primers 25 anys, la Fundació BM Granollers ha impulsat nombroses activitats per a la promoció de l’handbol en les etapes formatives. A més, ha donat suport a l’handbol femení, integrant el projecte d'esport i igualtat de gènere de l'Ajuntament de Granollers. Aquest projecte busca augmentar la participació de nenes, noies i dones en l’esport i l'activitat física, tot promovent relacions més justes entre homes i dones.",
+        "La Fundació també ha treballat per promoure el coneixement i la pràctica de l'handbol als centres educatius, ha posat en marxa un equip d'handbol inclusiu i ha impulsat amb el club un torneig de minihandbol i un torneig internacional d'handbol, amb la participació de jugadors i jugadores d'arreu del món.",
+      ],
+    },
+  ],
+};
