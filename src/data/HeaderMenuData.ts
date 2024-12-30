5f6519e4887dd43c64cd418f73ef29a945427d9b
fabianlopezdev <fabianlopez@pm.me>
2024-12-30 17:27:37 -0500

    Header Menu: added submenu of palmares del planter

--- src/data/HeaderMenuData.ts
+++ src/data/HeaderMenuData.ts
@@ -13,26 +13,68 @@ export const HEADER_MENU_LINKS = [
     label: "Palmarès del planter",
     url: "/palmares-del-planter",
     ariaLabel: "",
+    nested: [
+      {
+        label: "Veure tot",
+        url: "/palmares-del-planter",
+        ariaLabel: "Ves a l'inici de la pàgina de palmarès del planter",
+      },
+      {
+        label: "Palmarès",
+        url: "/palmares-del-planter#palmares",
+        ariaLabel: "Ves a la secció del palmarès de la pàgina palmarès del planter",
+      },
+      {
+        label: "Campionats de Catalunya",
+        url: "/palmares-del-planter#campionats-de-catalunya",
+        ariaLabel: "Ves a la secció de campionats de Catalunya de la pàgina palmarès del planter",
+      },
+      {
+        label: "La generació d'or",
+        url: "/palmares-del-planter#la-generacio-dor",
+        ariaLabel: "Ves a la secció de la generació d'or de la pàgina palmarès del planter",
+      },
+      {
+        label: "Campiones de Catalunya",
+        url: "/palmares-del-planter#campiones-de-catalunya-i-subcampiones-despanya",
+        ariaLabel: "Ves a la secció de campiones de Catalunya i subcampiones d'Espanya de la pàgina palmarès del planter",
+      },
+      {
+        label: "Subcampiones d'Espanya",
+        url: "/palmares-del-planter#campiones-de-catalunya-i-subcampiones-despanya",
+        ariaLabel: "Ves a la secció de campiones de Catalunya i subcampiones d'Espanya de la pàgina palmarès del planter",
+      },
+      {
+        label: "Subcampiones de Catalunya",
+        url: "/palmares-del-planter#subcampiones-de-catalunya",
+        ariaLabel: "Ves a la secció de subcampiones de Catalunya de la pàgina palmarès del planter",
+      },
+    ],
   },
   {
     label: "Tornejos",
     nested: [
       {
+        label: "Veure tot",
+        url: "/tornejos",
+        ariaLabel: "Ves a l'inici de la pàgina de tornejos",
+      },
+      {
         label: "Granollers Cup",
         url: "/tornejos#granollers-cup",
-        ariaLabel: "",
+        ariaLabel: "Ves a la secció de la Granollers Cup de la pàgina de tornejos",
       },
       {
         label: "Handbol escolar",
         url: "/tornejos#handbol-escolar",
-        ariaLabel: "",
+        ariaLabel: "Ves a la secció d'handbol escolar de la pàgina de tornejos",
       },
       {
         label: "Minihandbol",
         url: "/tornejos#minihandbol",
-        ariaLabel: "",
+        ariaLabel: "Ves a la secció de minihandbol de la pàgina de tornejos",
       },
-    ],
+    ]
   },
   {
     label: "Equip special",
@@ -43,16 +85,21 @@ export const HEADER_MENU_LINKS = [
     label: "Projectes de cooperació",
     nested: [
       {
+        label: "Veure tot",
+        url: "/projectes-de-cooperacio",
+        ariaLabel: "Ves a l'inici de la pàgina de projectes de cooperació",
+      },
+      {
         label: "Nigèria",
         url: "/projectes-de-cooperacio#nigeria",
-        ariaLabel: "",
+        ariaLabel: "Ves a la secció de cooperació en Nigèria de la pàgina de projectes de cooperació",
       },
       {
         label: "Senegal",
         url: "/projectes-de-cooperacio#senegal",
-        ariaLabel: "",
+        ariaLabel: "Ves a la secció de cooperació en Senegal de la pàgina de projectes de cooperació",
       },
-    ],
+    ]
   },
   {
     label: "La residència",
